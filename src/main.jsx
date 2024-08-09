4857e013698f60aaa6c3fd890b36c1c1009a15df
izi <inmz02@outlook.com>
2024-08-09 22:48:34 +0100

    Error Handling 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './styles/main.scss'
 
-import MainContainer from "./myComps/mainContainer.jsx";
+import MainContainer from "./myComps/MainContainer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
