bea5a773b8b21d721c20813593f46fa0fc3bf85a
izi <inmz02@outlook.com>
2024-08-09 22:42:55 +0100

    Error handling 1

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import './styles/main.scss'
 
-import MainContainer from './myComps/mainContainer.jsx'
-// import Clock from './myComps/Clock.jsx'
+import MainContainer from '../src/myComps/mainContainer.jsx'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <>
-      {/* <Clock/> */}
       <MainContainer>
         <App />
       </MainContainer>
